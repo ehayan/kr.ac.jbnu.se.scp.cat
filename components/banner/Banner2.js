18159c9c940db66c82644ff89eef841446306447
hayan <hayan8029@gmail.com>
2022-04-12 19:57:14 +0900

    main page

--- components/banner/Banner2.js
+++ components/banner/Banner2.js
@@ -1,36 +1,34 @@
-import React from "react";
-import Link from "next/link";
-import { Container, Row, Col } from "reactstrap";
-import Image from "next/image";
-import bannerimg from "../../assets/images/landingpage/banner-img.png";
+import React from 'react';
+import Link from 'next/link';
+import { Container, Row, Col } from 'reactstrap';
+import Image from 'next/image';
+import bannerimg from '../../assets/images/logos/cat_logo.png';
 
 const Banner2 = () => {
   return (
-    <div className="static-slider-head banner2">
+    <div className='static-slider-head banner2'>
       <Container>
-        <Row className="">
-          <Col lg="6" md="6" className="align-self-center">
-            <h1 className="title">
-              Build stunning websites with NextJs UI Kit
-            </h1>
-            <h4 className="subtitle font-light">
-              Free Reactstrap UI Kit with Lots of
-              <br /> Ready to Use Sections
+        <Row className=''>
+          <Col lg='6' md='6' className='align-self-center'>
+            <h1 className='title'>안녕하세요</h1>
+            <h4 className='subtitle font-light'>
+              협업 지원 도구 통합
+              <br /> Collaborate Assitant Tools
             </h4>
-            <a
-              href="https://wrappixel.com/templates/nextkit-nextjs-free-uikit"
-              className="btn btn-danger m-r-20 btn-md m-t-30 "
+            {/* <a
+              href='https://wrappixel.com/templates/nextkit-nextjs-free-uikit'
+              className='btn btn-danger m-r-20 btn-md m-t-30 '
             >
               Download Free
             </a>
-            <Link href="/#coming">
-              <a className="btn btn-md m-t-30  btn-outline-light ">
+            <Link href='/#coming'>
+              <a className='btn btn-md m-t-30  btn-outline-light '>
                 Upgrade To Pro
-              </a>
-            </Link>
+              </a> */}
+            {/* </Link> */}
           </Col>
-          <Col lg="6" md="6">
-            <Image src={bannerimg} alt="hero banner" />
+          <Col lg='6' md='6'>
+            <Image src={bannerimg} alt='hero banner' />
           </Col>
         </Row>
       </Container>
--- layout/header/Header.js
+++ layout/header/Header.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import {
   Collapse,
   Navbar,
@@ -11,65 +11,65 @@ import {
   NavItem,
   Container,
   NavLink,
-} from "reactstrap";
-import logo from "../../assets/images/logos/white-text.png";
+} from 'reactstrap'; //module
+import logo from '../../assets/images/logos/small_cat_logo.png';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   const toggle = () => setIsOpen(!isOpen);
   return (
-    <div className="topbar" id="top">
-      <div className="header6">
-        <Container className="po-relative">
-          <Navbar className="navbar-expand-lg h6-nav-bar">
-            <NavbarBrand href="/">
-              <Image src={logo} alt="wrapkit" />
+    <div className='topbar' id='top'>
+      <div className='header6'>
+        <Container className='po-relative'>
+          <Navbar className='navbar-expand-lg h6-nav-bar'>
+            <NavbarBrand href='/'>
+              <Image src={logo} alt='wrapkit' />
             </NavbarBrand>
             <NavbarToggler onClick={toggle}>
-              <span className="ti-menu"></span>
+              <span className='ti-menu'></span>
             </NavbarToggler>
             <Collapse
               isOpen={isOpen}
               navbar
-              className="hover-dropdown ml-auto"
-              id="h6-info"
+              className='hover-dropdown ml-auto'
+              id='h6-info'
             >
-              <Nav navbar className="ml-auto">
-                <NavItem>
-                  <Link href="/">
+              <Nav navbar className='ml-auto'>
+                {/* <NavItem>
+                  <Link href='/'>
                     <a
                       className={
-                        router.pathname == "/"
-                          ? "text-white nav-link"
-                          : "nav-link"
+                        router.pathname == '/'
+                          ? 'text-white nav-link'
+                          : 'nav-link'
                       }
                     >
                       Custom Components
                     </a>
                   </Link>
-                </NavItem>
+                </NavItem> */}
                 <NavItem>
-                  <Link href="/basic">
+                  <Link href='/basic'>
                     <a
                       className={
-                        router.pathname == "/basic"
-                          ? "text-white nav-link"
-                          : "nav-link"
+                        router.pathname == '/basic'
+                          ? 'text-white nav-link'
+                          : 'nav-link'
                       }
                     >
-                      Basic-Components
+                      Tool Help
                     </a>
                   </Link>
                 </NavItem>
               </Nav>
-              <div className="act-buttons">
+              <div className='act-buttons'>
                 <NavLink
-                  href="https://wrappixel.com/templates/nextkit-nextjs-free-uikit"
-                  className="btn btn-light font-14"
-                  target="_blank"
+                  // href='https://wrappixel.com/templates/nextkit-nextjs-free-uikit'
+                  className='btn btn-light font-14'
+                  target='_blank'
                 >
-                  Download Free
+                  Login
                 </NavLink>
               </div>
             </Collapse>
