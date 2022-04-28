298176326e9dd1e7455702ae97cbee348bd2cbea
KHW <hynwoo6901@naver.com>
2022-04-28 18:44:33 +0900

    apply github function:gitcommitcomponent.js

--- api/github.js
+++ api/github.js
@@ -1,3 +1,17 @@
+// return repository name from github link
+export function getRepositoryName(link) {
+  return link.split("/")[4];
+}
+
+/*
+  return 3 recent datas of commit from github link
+  data = {
+    name : author name,
+    avatar_url : author avatar image url,
+    date : commit date
+    message : commit message
+  }
+*/
 export async function getCommits(link) {
   const owner = link.split("/")[3];
   const repos = link.split("/")[4];
@@ -6,9 +20,15 @@ export async function getCommits(link) {
     `https://api.github.com/repos/${owner}/${repos}/commits`
   );
   const json = await response.json();
-  console.log(json);
-  const returns = {
-    committer : json[0].committer.login,
-  };
+  let returns = [];
+  for(let i=0; i<3; i++) {
+    const value = {
+      name : json[i].commit.author.name,
+      avatar_url : json[i].author.avatar_url,
+      date : json[i].commit.author.date,
+      message : json[i].commit.message
+    }
+    returns.push(value);
+  }
   return returns;
 }
--- components/custom/sections/gitcommitcomponent.js
+++ components/custom/sections/gitcommitcomponent.js
@@ -5,12 +5,20 @@
 // commit message => commit message
 
 /* eslint-disable */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import GitLogo from "../../../assets/images/tool-logos/small-git-hub.png";
 import { Row, Col, Container, Card, CardBody } from "reactstrap";
+import * as Github from "../../../api/github";
 
 const GitCommitComponent = () => {
+  const link = "https://github.com/gusdn6901/kr.ac.jbnu.se.scp.cat";
+  const [commits, setCommits] = useState([]);
+  useEffect(()=> {
+    Github.getCommits(link).then((data)=> {
+      setCommits(data);
+    });
+  }, []);
   return (
     <div>
       <div className="pricing8 spacer b-t">
@@ -18,7 +26,7 @@ const GitCommitComponent = () => {
           <Row className="justify-content-center">
             <Col md="7" className="text-center">
               <h2 className="title">
-                Current commits of 'A' repository
+                Current commits of '{Github.getRepositoryName(link)}' repository
               </h2>
               <h6 className="subtitle">
                 가장 최근에 푸시된 커밋과 관련된 정보를 확인할 수 있습니다.
@@ -26,20 +34,47 @@ const GitCommitComponent = () => {
               <span className="subtitle">
                 지정된 리포지토리 :&nbsp;
               </span>
-              <a href="/">
-                repository link
+              <a href={link}>
+                {link}
               </a>
             </Col>
           </Row>
           <Row className="m-t-40">
-            <Col md="4" className="ml-auto pricing-box align-self-center">
+            {commits.map((commit) => (
+              <Col md="4" className="ml-auto pricing-box align-self-center">
               <Card className="b-all">
                 <p className="commit-txt btn-info p-15 btn-arrow btn-block">
-                  commit date
+                {commit.date.split('T')[0]} {commit.date.split('T')[1].split('Z')[0]}
                 </p>
                 <CardBody className="p-30 text-center">
-                  <Image src={GitLogo}/>
-                  <h5>commiter</h5>
+                  <div
+                   style={{
+                     borderRadius:'50%',
+                     border:"2px solid white",
+                     display: "inline-block",
+                     width: 70,
+                     height: 70,
+                     overflow: "hidden"
+                    }}
+                   >
+                    <Image src={commit.avatar_url} width="70" height="70" objectFit="cover" />
+                  </div>
+                  <h5>{commit.name}</h5>
+                  <p className="m-t-40">
+                    {commit.message}
+                  </p>
+                </CardBody>
+              </Card>
+            </Col>
+            ))}
+            {/* <Col md="4" className="ml-auto pricing-box align-self-center">
+              <Card className="b-all">
+                <p className="commit-txt btn-info p-15 btn-arrow btn-block">
+                date
+                </p>
+                <CardBody className="p-30 text-center">
+                  <Image src={GitLogo} />
+                  <h5>name</h5>
                   <p className="m-t-40">
                     commit message
                   </p>
@@ -49,11 +84,11 @@ const GitCommitComponent = () => {
             <Col md="4" className="ml-auto pricing-box align-self-center">
               <Card className="b-all">
                 <p className="commit-txt btn-danger p-15 btn-arrow btn-block">
-                  commit date
+                date
                 </p>
                 <CardBody className="p-30 text-center">
                   <Image src={GitLogo}/>
-                  <h5>commiter</h5>
+                  <h5>name</h5>
                   <p className="m-t-40">
                     commit message
                   </p>
@@ -63,17 +98,17 @@ const GitCommitComponent = () => {
             <Col md="4" className="ml-auto pricing-box align-self-center">
               <Card className="b-all">
                 <p className="commit-txt btn-info p-15 btn-arrow btn-block">
-                  commit date
+                date
                 </p>
                 <CardBody className="p-30 text-center">
                   <Image src={GitLogo}/>
-                  <h5>commiter</h5>
+                  name
                   <p className="m-t-40">
                     commit message
                   </p>
                 </CardBody>
               </Card>
-            </Col>
+            </Col> */}
           </Row>
         </Container>
       </div>
