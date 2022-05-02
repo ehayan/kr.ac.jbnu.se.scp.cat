2bc15b97a6d6ed6d887b6dd301bc29b18ff857d5
leeyj825 <uh5213@naver.com>
2022-05-03 04:03:50 +0900

    add clone code part // copy part is not yet,,, not perfect,,,

--- components/custom/sections/gitcommitcomponent.js
+++ components/custom/sections/gitcommitcomponent.js
@@ -10,23 +10,54 @@ import Image from "next/image";
 import GitLogo from "../../../assets/images/tool-logos/small-git-hub.png";
 import { Row, Col, Container, Card, CardBody } from "reactstrap";
 import * as Github from "../../../api/github";
+import { BiCopy } from 'react-icons/bi';
 
 const GitCommitComponent = () => {
   const link = "https://github.com/gusdn6901/kr.ac.jbnu.se.scp.cat";
   const [commits, setCommits] = useState([]);
+  const [cloneBtn, setCloneBtn] = useState(0);
+  
   useEffect(()=> {
     Github.getCommits(link).then((data)=> {
       setCommits(data);
     });
+    setCloneBtn(1);
   }, []);
+
+  function cloneAddress(){
+    const address = "";
+    const ownerName = Github.getOwnerName(link);
+    const repositoryName = Github.getRepositoryName(link);
+    if(cloneBtn === 1){
+      address = `https://github.com/${ownerName}/${repositoryName}`
+    }
+    else if (cloneBtn === 2){
+      address = `git@github.com:${ownerName}/${repositoryName}`
+    }
+    else if(cloneBtn === 3){
+      address = `gh repo clone ${ownerName}/${repositoryName}`
+    }
+    
+    return address;
+  }
+
+  function copyToClipboard() {
+    if(typeof window !== 'object') return;
+    const copyText = document.querySelector("#clone-address");
+    copyText.select();
+    document.execCommand("Copy");
+    console.log('Copied!');
+  }
+
   return (
     <div>
       <div className="pricing8 spacer b-t">
         <Container>
           <Row className="justify-content-center">
-            <Col md="7" className="text-center">
+            <Col md="6" className="text-center">
               <h2 className="title">
-                Current commits of '{Github.getRepositoryName(link)}' repository
+                <span>Current commits of </span>
+                <p>'{Github.getRepositoryName(link)}' repository</p>
               </h2>
               <h6 className="subtitle">
                 가장 최근에 푸시된 커밋과 관련된 정보를 확인할 수 있습니다.
@@ -37,6 +68,35 @@ const GitCommitComponent = () => {
               <a href={link}>
                 {link}
               </a>
+            </Col>
+            <Col md="6" className="text-center">
+              <h2 className="title">
+                Clone
+              </h2>
+              <div>
+                <button id="clone-https" className="btn-github" onClick={() => {setCloneBtn(1); }}> HTTPS </button>&ensp;
+                <button id="clone-ssh" className="btn-github" onClick={() => {setCloneBtn(2); }}> SSH </button>&ensp;
+                <button id="clone-cli" className="btn-github" onClick={() => {setCloneBtn(3); }}> GitHub CLI </button>
+              </div>
+              <div>
+                <input id="clone-address" type="text"  readOnly value={cloneAddress()}/>
+                <button className="btn-copy" onClick={copyToClipboard()}><BiCopy/></button>
+                <style jsx>{`
+                  input{
+                    border-radius: 5px;
+                    border-top-right-radius: 0px;
+                    border-bottom-right-radius: 0px;
+                    margin-top: 10px;
+                    margin-left: 15px;
+                    width: 230px;
+                    font-size:15px;
+                  }
+                  button{
+                    margin-top: 10px;
+                    height:28px;
+                  }
+                `}</style>
+              </div>
             </Col>
           </Row>
           <Row className="m-t-40">
