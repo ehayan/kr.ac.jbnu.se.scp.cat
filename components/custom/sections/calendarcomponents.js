b4ffcc77c9ad358e2bbeca96b909e137afaed079
leeyj825 <uh5213@naver.com>
2022-05-08 23:43:33 +0900

    calendar error

--- components/custom/sections/calendarcomponents.js
+++ components/custom/sections/calendarcomponents.js
@@ -1,44 +1,89 @@
 import FullCalendar from "@fullcalendar/react";
+import { Calendar } from "@fullcalendar/core";
 import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import { useRef } from "react";
+import { useEffect } from "react";
 
-const Calendar = () => {
+const CalendarFunction = () => {
   const calendarRef = useRef(null);
 
-  
-  function selectEvent(arg) {
-    console.log(arg.start + arg.end)
-    const title = prompt('Event Title:');
-    const calendar = new FullCalendar.Calendar();
-      if (title) {
+  const calendarEl = document.getElementById('calendar');
+  const calendar = new Calendar(calendarEl, {
+    plugins:[dayGridPlugin, interactionPlugin],
+    editable: true,
+    selectable: true,
+    headerToolbar : {
+      left : 'prevYear,prev,next,nextYear',
+      center : 'title',
+      right : 'today dayGridMonth,dayGridWeek,dayGridDay'
+    },
+    select: (arg) => {
+      const title = prompt('Event Title:');
+      if(title){
         calendar.addEvent({
-          title: title,
-          start: arg.start,
-          end: arg.end,
-          allDay: arg.allDay
+          title : title,
+          start : arg.start,
+          end : arg.end,
+          allDay : arg.allDay,
         })
-      };
-    calendar.unselect();
-  }
-
-  return (
-    <FullCalendar
-      innerRef={calendarRef}
-      plugins={[dayGridPlugin, interactionPlugin]}
-      editable
-      selectable
-      select ={selectEvent}
-      headerToolbar = {
-        {
-          left : 'prevYear,prev,next,nextYear',
-          center : 'title',
-          right : 'today dayGridMonth,dayGridWeek,dayGridDay'
-        }
       }
-      
-    />
-  );
+    }
+  });
 };
 
-export default Calendar;
+//   return (
+//     <FullCalendar
+//       innerRef={calendarRef}
+//       plugins={[dayGridPlugin, interactionPlugin]}
+//       editable
+//       selectable
+//       unselectAuto
+//       select={selectEvent}
+//       headerToolbar = {
+//         {
+//           left : 'prevYear,prev,next,nextYear',
+//           center : 'title',
+//           right : 'today dayGridMonth,dayGridWeek,dayGridDay'
+//         }
+//       }
+//       events={[
+
+//       ]}
+//     />
+//   );
+// };
+
+export default CalendarFunction;
+
+// const CalendarFunction = () => {
+//   if (typeof document !== 'undefined') {
+//   const documentRef = useRef(document);
+//   const calendarEl = document.getElementById('calendar');
+//         const calendar = new Calendar(calendarEl, {
+//           plugins : [dayGridPlugin, interactionPlugin],
+//             // editable
+//             selectable : true,
+//             headerToolbar : {
+//               left : 'prevYear,prev,next,nextYear',
+//               center : 'title',
+//               right : 'today dayGridMonth,dayGridWeek,dayGridDay'
+//             },
+//             // select : (arg) => {
+//             //   const title = prompt('일정을 입력해주세요.');
+//             //   if(title){
+//             //     calendar.addEvent({
+//             //       title: title,
+//             //       start: arg.start,
+//             //       end: arg.end,
+//             //       allDay: arg.allDay,
+//             //     })
+//             //   }
+//             //   calendar.unselect();
+//             // }
+//           });
+//       calendar.render();
+//   }
+// };
+
+// export default CalendarFunction;
