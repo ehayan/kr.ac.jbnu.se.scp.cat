3f2da50b45624ead3d80ed4a0afecc7a615aea3c
leeyj825 <uh5213@naver.com>
2022-05-14 02:35:43 +0900

    modify trello page

--- components/custom/sections/trelloboardcomponent.js
+++ components/custom/sections/trelloboardcomponent.js
@@ -1,10 +1,8 @@
 /* eslint-disable */
-import React, { useEffect, useState } from "react";
 import { Row, Col, Container, Card, CardBody } from "reactstrap";
-import * as Github from "../../../api/github";
 
 const TrelloBoardComponent = () => {
-  
+
   return (
     <div>
       <div>
@@ -16,15 +14,15 @@ const TrelloBoardComponent = () => {
           
           <Row className="m-t-30">
             <Col md="2" className="m-l-20 align-self-center">
-              <Card className="board-card  m-b-0 p-0 b-all">
+              <Card className="board-card card-width-300 m-b-0 p-0 b-all">
                 <p className="board-card-name m-b-0 ">To Do	&#40;requirments&#41;</p>
                 <CardBody>
-                  <Card className="card-width b-all">
+                  <Card className="card-width-270 b-all">
                       <CardBody >
                           <p className="board-card-content">트렐로/노션/슬랙</p>
                       </CardBody>
                   </Card>
-                  <Card className="card-width b-all">
+                  <Card className="card-width-270 b-all">
                       <CardBody >
                           <p className="board-card-content">대시보드&#40;캘린더/참여자추가&#41;</p>
                       </CardBody>
@@ -33,15 +31,15 @@ const TrelloBoardComponent = () => {
               </Card>
             </Col>
             <Col md="2" className="m-l-20 align-self-center">
-              <Card className="board-card m-b-0 p-0 b-all">
+              <Card className="board-card card-width-300 m-b-0 p-0 b-all">
                 <p className="board-card-name m-b-0 ">Done	&#40;requirments&#41;</p>
                 <CardBody>
-                  <Card className="card-width b-all">
+                  <Card className="card-width-270 b-all">
                       <CardBody >
                           <p className="board-card-content">깃허브 연동</p>
                       </CardBody>
                   </Card>
-                  <Card className="card-width b-all">
+                  <Card className="card-width-270 b-all">
                       <CardBody >
                           <p className="board-card-content">구글 계정 로그인</p>
                       </CardBody>
@@ -50,10 +48,10 @@ const TrelloBoardComponent = () => {
               </Card>
             </Col>
             <Col md="2" className="m-l-20 align-self-center">
-              <Card className="board-card m-b-0 p-0 b-all">
+              <Card className="board-card card-width-300 m-b-0 p-0 b-all">
                 <p className="board-card-name m-b-0 ">To Do	&#40;assignments&#41;</p>
                 <CardBody>
-                  <Card className="card-width b-all">
+                  <Card className="card-width-270 b-all">
                       <CardBody >
                           <p className="board-card-content">12차 과제&#40;과제 진행 사항 발표&#41;</p>
                       </CardBody>
@@ -62,15 +60,15 @@ const TrelloBoardComponent = () => {
               </Card>
             </Col>
             <Col md="2" className="m-l-20 align-self-center">
-              <Card className="board-card m-b-0 p-0 b-all">
+              <Card className="board-card card-width-300 m-b-0 p-0 b-all">
                 <p className="board-card-name m-b-0 ">Done	&#40;assignments&#41;</p>
                 <CardBody>
-                  <Card className=" card-width b-all">
+                  <Card className=" card-width-270 b-all">
                       <CardBody >
                           <p className="board-card-content">1차 과제&#40;브레인스토밍&#41;</p>
                       </CardBody>
                   </Card>
-                  <Card className=" card-width b-all">
+                  <Card className=" card-width-270 b-all">
                       <CardBody >
                           <p className="board-card-content">2차 과제&#40;아이디어 선정&#41;</p>
                       </CardBody>
