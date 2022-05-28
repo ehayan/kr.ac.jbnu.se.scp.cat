8188a3f8721f1b456c1cff78f50336938caf9712
leeyj825 <uh5213@naver.com>
2022-05-28 17:50:03 +0900

    add link with firebase

--- layout/header/Header.js
+++ layout/header/Header.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 // import GithubDetailsMenuElement from'@github/details-menu-element';
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import {
   Collapse,
   Navbar,
@@ -12,10 +12,10 @@ import {
   NavItem,
   Container,
   NavLink,
-} from 'reactstrap'; //module
-import { signIn, signOut, useSession } from 'next-auth/react';
+} from "reactstrap"; //module
+import { signIn, signOut, useSession } from "next-auth/react";
 // import { IoMail, IoMailUnread} from 'react-icons/io5';
-import logo from '../../assets/images/logos/probby_logo.png';
+import logo from "../../assets/images/logos/probby_logo.png";
 
 const Header = () => {
   const { data: session, status } = useSession();
@@ -24,36 +24,32 @@ const Header = () => {
   const router = useRouter();
   const toggle = () => setIsOpen(!isOpen);
 
-  if (status === 'loading') {
-    return <p>Loading...</p>;
-  }
-
   return (
-    <div className='topbar' id='top'>
-      <div className='header6'>
-        <Container className='po-relative'>
-          <Navbar className='navbar-expand-lg h6-nav-bar'>
-            <NavbarBrand href='/'>
-              <Image src={logo} width='125px' height='70px' />
+    <div className="topbar" id="top">
+      <div className="header6">
+        <Container className="po-relative">
+          <Navbar className="navbar-expand-lg h6-nav-bar">
+            <NavbarBrand href="/">
+              <Image src={logo} width="125px" height="70px" />
             </NavbarBrand>
 
             <NavbarToggler onClick={toggle}>
-              <span className='ti-menu'></span>
+              <span className="ti-menu"></span>
             </NavbarToggler>
             <Collapse
               isOpen={isOpen}
               navbar
-              className='hover-dropdown ml-auto'
-              id='h6-info'
+              className="hover-dropdown ml-auto"
+              id="h6-info"
             >
-              <Nav navbar className='ml-auto'>
-                <NavItem className={session ? '' : 'hide'}>
-                  <Link href='/project-dashboard'>
+              <Nav navbar className="ml-auto">
+                <NavItem className={session ? "" : "hide"}>
+                  <Link href="/project-dashboard">
                     <a
                       className={
-                        router.pathname == '/project'
-                          ? 'text-white nav-link'
-                          : 'nav-link'
+                        router.pathname == "/project"
+                          ? "text-white nav-link"
+                          : "nav-link"
                       }
                     >
                       Project
@@ -61,12 +57,12 @@ const Header = () => {
                   </Link>
                 </NavItem>
                 <NavItem>
-                  <Link href='/toolhelp/github'>
+                  <Link href="/toolhelp/github">
                     <a
                       className={
-                        router.pathname == '/toolhelp/github'
-                          ? 'text-white nav-link'
-                          : 'nav-link'
+                        router.pathname == "/toolhelp/github"
+                          ? "text-white nav-link"
+                          : "nav-link"
                       }
                     >
                       Tool Help
@@ -74,18 +70,18 @@ const Header = () => {
                   </Link>
                 </NavItem>
               </Nav>
-              <div className='act-buttons'>
+              <div className="act-buttons">
                 <button
-                  className='btn btn-light font-14'
+                  className="btn btn-light font-14"
                   onClick={() => {
                     if (session) {
                       signOut();
                     } else if (!session) {
-                      signIn('google');
+                      signIn("google");
                     }
                   }}
                 >
-                  {session ? 'Logout' : 'Login'}
+                  {session ? "Logout" : "Login"}
                 </button>
               </div>
             </Collapse>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
-import Head from 'next/head';
-import CustomComponents from '../components/custom/Custom-components';
-import AfterLoginBanner from '../components/banner/AfterLoginBanner';
-import BeforeLoginBanner from '../components/banner/BeforeLoginBanner';
-import Project_Setting from '../components/custom/sections/settingcomponent';
-import { useSession } from 'next-auth/react';
+import Head from "next/head";
+import CustomComponents from "../components/custom/Custom-components";
+import AfterLoginBanner from "../components/banner/AfterLoginBanner";
+import BeforeLoginBanner from "../components/banner/BeforeLoginBanner";
+import Project_Setting from "../components/custom/sections/settingcomponent";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
+
   return (
     <div>
       <Head>
         <title>PROBBY</title>
-        <meta name='description' content='CAT' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="CAT" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       {session ? <AfterLoginBanner /> : <BeforeLoginBanner />}
       {session ? (
-        <div className='spacer'>
-          <h2 className='text-center'>PROJECT LIST</h2>
-          <p className='text-center'>프로젝트 관리(등록/수정/삭제/추가)</p>
+        <div className="spacer">
+          <h2 className="text-center">PROJECT LIST</h2>
+          <p className="text-center">프로젝트 관리(등록/수정/삭제/추가)</p>
           <Project_Setting />
         </div>
       ) : (
