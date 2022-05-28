f79dbc6df8bf1319f8c58ea20085711c87c31e71
leeyj825 <uh5213@naver.com>
2022-05-28 18:23:48 +0900

    delete project button on header // after push enter button, can go to dashboard page

--- layout/header/Header.js
+++ layout/header/Header.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 // import GithubDetailsMenuElement from'@github/details-menu-element';
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import {
   Collapse,
   Navbar,
@@ -12,11 +12,11 @@ import {
   NavItem,
   Container,
   NavLink,
-} from 'reactstrap'; //module
-import { signIn, signOut, useSession } from 'next-auth/react';
+} from "reactstrap"; //module
+import { signIn, signOut, useSession } from "next-auth/react";
 // import { IoMail, IoMailUnread} from 'react-icons/io5';
-import logo from '../../assets/images/logos/probby_logo.png';
-import { Button } from 'react-bootstrap';
+import logo from "../../assets/images/logos/probby_logo.png";
+import { Button } from "react-bootstrap";
 
 const Header = () => {
   const { data: session, status } = useSession();
@@ -25,49 +25,49 @@ const Header = () => {
   const router = useRouter();
   const toggle = () => setIsOpen(!isOpen);
 
-  if (status === 'loading') {
+  if (status === "loading") {
     return <p>Loading...</p>;
   }
 
   return (
-    <div className='topbar' id='top'>
-      <div className='header6'>
-        <Container className='po-relative'>
-          <Navbar className='navbar-expand-lg h6-nav-bar'>
-            <NavbarBrand href='/'>
-              <Image src={logo} width='125px' height='70px' />
+    <div className="topbar" id="top">
+      <div className="header6">
+        <Container className="po-relative">
+          <Navbar className="navbar-expand-lg h6-nav-bar">
+            <NavbarBrand href="/">
+              <Image src={logo} width="125px" height="70px" />
             </NavbarBrand>
 
             <NavbarToggler onClick={toggle}>
-              <span className='ti-menu'></span>
+              <span className="ti-menu"></span>
             </NavbarToggler>
             <Collapse
               isOpen={isOpen}
               navbar
-              className='hover-dropdown ml-auto'
-              id='h6-info'
+              className="hover-dropdown ml-auto"
+              id="h6-info"
             >
-              <Nav navbar className='ml-auto'>
-                <NavItem className={session ? '' : 'hide'}>
-                  <Link href='/project-dashboard'>
+              <Nav navbar className="ml-auto">
+                {/* <NavItem className={session ? "" : "hide"}>
+                  <Link href="/project-dashboard">
                     <a
                       className={
-                        router.pathname == '/project'
-                          ? 'text-white nav-link'
-                          : 'nav-link'
+                        router.pathname == "/project"
+                          ? "text-white nav-link"
+                          : "nav-link"
                       }
                     >
                       Project
                     </a>
                   </Link>
-                </NavItem>
+                </NavItem> */}
                 <NavItem>
-                  <Link href='/toolhelp/github'>
+                  <Link href="/toolhelp/github">
                     <a
                       className={
-                        router.pathname == '/toolhelp/github'
-                          ? 'text-white nav-link'
-                          : 'nav-link'
+                        router.pathname == "/toolhelp/github"
+                          ? "text-white nav-link"
+                          : "nav-link"
                       }
                     >
                       Tool Help
@@ -75,24 +75,24 @@ const Header = () => {
                   </Link>
                 </NavItem>
               </Nav>
-              <div className='act-buttons'>
+              <div className="act-buttons">
                 <Button
-                  variant='outline-warning'
+                  variant="outline-warning"
                   onClick={() => {
                     if (session) {
                       signOut();
                     } else if (!session) {
-                      signIn('google');
+                      signIn("google");
                     }
                   }}
                 >
-                  {session ? 'Logout' : 'Login'}
+                  {session ? "Logout" : "Login"}
                 </Button>
               </div>
             </Collapse>
             <style jsx>{`
               a {
-                font-family: 'Baskin_R';
+                font-family: "Baskin_R";
                 font-size: 15px;
               }
             `}</style>
