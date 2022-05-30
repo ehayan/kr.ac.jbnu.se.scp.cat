97a0c5c92aade202a56477546631d015eeb1bc41
wghwgh12 <wghwgh12@naver.com>
2022-05-30 19:25:47 +0900

    change next-auth, add mongodb-connection

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,39 @@
 //for google login and logout.
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export default async function auth(req, res) {
+    return await NextAuth(req, res, {
+      adapter: MongoDBAdapter(
+         clientPromise
+      ),
+      providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_ID,
+          clientSecret: process.env.GOOGLE_SECRET,
+          authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+        }),
+      ],
+      callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+          return true;
+        },
+        async session({ session, token, user }) {
+          session.user._id = user.id;
+          return session;
+        },
+        async jwt(token, account){
+            if(account?.accessToken){
+                token.accessToken = account.accessToken;
+            }
+            return token;
+        },
+    },
+    });
+}
+/*export default NextAuth({
     // Configure one or more authentication providers
     providers: [
       GoogleProvider({
@@ -10,6 +41,7 @@ export default NextAuth({
           clientSecret: process.env.GOOGLE_SECRET,
           authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
       })
+      
       // ...add more providers here
     ],
     jwt : {
@@ -30,6 +62,6 @@ export default NextAuth({
             return Promise.resolve('/');
         }
     }
-  });
+  });*/
 
   
