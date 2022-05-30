646e71f8eb74476b6577951806a4a9c8e18d0997
leeyj825 <uh5213@naver.com>
2022-05-30 19:38:01 +0900

    add slack input

---
+++ components/custom/sections/slackaddcomponent.js
@@ -0,0 +1,75 @@
+import { Form, Input, Row, Col, Card, CardText, Container } from "reactstrap";
+import { GoTriangleDown } from "react-icons/go";
+import Image from "next/image";
+import member1 from "../../../assets/images/slack-member/ex1.png";
+import member2 from "../../../assets/images/slack-member/ex2.png";
+import member3 from "../../../assets/images/slack-member/ex3.png";
+import { useState } from "react";
+
+const SlackComponent = () => {
+  const [message, setMessage] = useState("");
+  const handleMessageInput = ({ target: { value } }) => {
+    setMessage(value);
+  };
+
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      <div className="align-self-center m-t-40">
+        <Container className="m-t-20 p-0">
+          <Row>
+            <Col md="12">
+              <Card body className="card-shadow m-b-0">
+                <p className="m-l-10 m-t-5 m-b-5">
+                  공지를 작성해 채널에 메시지를 보내세요.
+                </p>
+                <p className="m-l-10 m-b-5">
+                  프로젝트 멤버가 보낸 메시지는 오른쪽에서 확인할 수 있습니다.
+                </p>
+              </Card>
+            </Col>
+            <Col md="12">
+              <Form className="m-t-10" onSubmit={handleSubmit}>
+                <div className="m-b-20 slack-input">
+                  <Input
+                    type="text"
+                    value={message}
+                    placeholder="Enter Message"
+                    className="font-15 "
+                    style={{ height: "200px" }}
+                    onChange={handleMessageInput}
+                  />
+                  <Input
+                    type="submit"
+                    value="Send"
+                    onClick={handleSubmit}
+                    style={{
+                      borderRadius: "10px",
+                    }}
+                    className="m-t-10 bg-info font-semibold font-16 btn-rounded text-uppercase text-black text-center"
+                  />
+                  {/* <button type='submit'>ADD</button> */}
+                </div>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <style jsx>{`
+        h2 {
+          font-family: "Spoca_B";
+        }
+        h3 {
+          font-family: "87MM";
+          font-size: 26px;
+        }
+        p {
+          font-family: "RIDI";
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default SlackComponent;
--- pages/project-slack.js
+++ pages/project-slack.js
@@ -1,14 +1,30 @@
-import Head from 'next/head';
-import SlackComponent from '../components/custom/sections/slackcomponent';
+import Head from "next/head";
+import { Row, Col, Container } from "reactstrap";
+import SlackListComponent from "../components/custom/sections/slacklistcomponent";
+import SlackAddComponent from "../components/custom/sections/slackaddcomponent";
 
 const Slack = (page) => {
   return (
     <div>
       <Head>
         <title>PROBBY | Slack</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SlackComponent />
+      <div className="m-l-30">
+        <h2 className="title font-bold">Slack</h2>
+        <p>URL : link </p>
+        {/* <a href={link}>{link}</a> */}
+      </div>
+      <Container>
+        <Row>
+          <Col md="6">
+            <SlackAddComponent />
+          </Col>
+          <Col md="6">
+            <SlackListComponent />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
