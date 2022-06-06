1230837191e84671909ce788d0512f7c41c1cab3
KHW <hynwoo6901@naver.com>
2022-06-07 07:02:35 +0900

    change after login banner to print user's name middle of page

--- components/banner/AfterLoginBanner.js
+++ components/banner/AfterLoginBanner.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Image from 'next/image';
 import bannerimg from '../../assets/images/logos/probby_logo.png';
+import { useSession } from 'next-auth/react';
 
 const Banner = () => {
+  const { data: session } = useSession();
   return (
     <div className='static-slider-head'>
       <Container>
         <Row>
           <Col className='align-self-center'>
-            <h1 className='title'>어서오세요 ##님 !</h1>
+            <h1 className='title'>어서오세요 {session.user.name}님 !</h1>
             <style jsx>{`
               h1 {
                 font-family: 'Lotte';
